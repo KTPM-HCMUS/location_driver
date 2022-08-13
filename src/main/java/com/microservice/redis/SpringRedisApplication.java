110c3088e82e468659a03719ef95fe5f9d758e34
pear_ngoc <lethanhngoc123a@gmail.com>
2022-08-13 10:47:04 +0700

    make it better

--- src/main/java/com/microservice/redis/SpringRedisApplication.java
+++ src/main/java/com/microservice/redis/SpringRedisApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
+
 @SpringBootApplication
 @RestController
 @RequestMapping("api/v1/location")
@@ -22,6 +24,11 @@ public class SpringRedisApplication {
     @GetMapping
     public Object getDriver(@RequestParam("longitude") double longitude, @RequestParam("latitude") double latitude){
         return locationDAO.getDriverNearest(new Location(longitude, latitude));
+    }
+
+    @GetMapping("/all")
+    public Set<Object> getAllMembers(){
+        return locationDAO.getAllDriverIsOnline();
     }
 
     public static void main(String[] args) {
